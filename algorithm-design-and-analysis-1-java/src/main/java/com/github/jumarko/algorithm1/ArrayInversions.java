a3a6dd4225dbec66145769f007adf5370c990f93
Juraj Martinka <juraj@snapshot.travel>
2016-07-07 13:18:00 +0200

    Algo I  - finish array inversions implementation in java - more efficient algorithm O(n log n)

--- algorithm-design-and-analysis-1-java/src/main/java/com/github/jumarko/algorithm1/ArrayInversions.java
+++ algorithm-design-and-analysis-1-java/src/main/java/com/github/jumarko/algorithm1/ArrayInversions.java
@@ -1,6 +1,5 @@
 package com.github.jumarko.algorithm1;
 
-import java.util.Arrays;
 
 /**
  * Encapsulates an algorithm for counting array inversions as demonstrated
@@ -21,10 +20,15 @@ public class ArrayInversions {
     }
 
     public static int countInversions(int[] numbers) {
+        return countAllInversions(numbers).count;
+    }
+
+    private static InversionsResult countAllInversions(int[] numbers) {
         if (numbers.length < 2) {
-            return 0;
+            return new InversionsResult(0, numbers);
         }
 
+        // split input into two arrays - Divide & Conquer
         int firstHalfSize = numbers.length / 2;
         final int secondHalfSize = numbers.length - firstHalfSize;
 
@@ -34,7 +38,58 @@ public class ArrayInversions {
         System.arraycopy(numbers, 0, firstHalf, 0, firstHalfSize);
         System.arraycopy(numbers, firstHalfSize, secondHalf, 0, secondHalfSize);
 
-        return countInversions(firstHalf) + countInversions(secondHalf);
+        // recursively compute all inversions - the primitive case (single or zero element array) means no inversion
+        // multi-elements array are split to the primitive cases and then,
+        // the split inversions are counted when two arrays are being merged into the destination much like in case
+        // of merge sort algorithm.
+        // Notice that we have to propagate sorted "destination" array as a result from this function
+        // so that it can be used in higher-level "merge".
+        final InversionsResult firstHalfInversions = countAllInversions(firstHalf);
+        final InversionsResult secondHalfInversions = countAllInversions(secondHalf);
+        final InversionsResult splitInversions = countSplitInversions(firstHalfInversions.sortedItems,
+                secondHalfInversions.sortedItems);
+
+        return new InversionsResult(firstHalfInversions.count + secondHalfInversions.count + splitInversions.count,
+                splitInversions.sortedItems);
+    }
+
+    private static InversionsResult countSplitInversions(int[] firstHalf, int[] secondHalf) {
+
+        int splitInversionsCount = 0;
+        int fIndex = 0;
+        int sIndex = 0;
+        int[] destination = new int[firstHalf.length + secondHalf.length];
+        for (int i = 0; i < destination.length; i++) {
+            if (fIndex < firstHalf.length && sIndex < secondHalf.length) {
+                if (firstHalf[fIndex] <= secondHalf[sIndex]) {
+                    destination[fIndex + sIndex] = firstHalf[fIndex];
+                    fIndex++;
+                } else {
+                    // split inversion - count is determined by the number of elements
+                    // in first half that have not been processed yet
+                    splitInversionsCount += firstHalf.length - fIndex;
+                    destination[fIndex + sIndex] = secondHalf[sIndex];
+                    sIndex++;
+                }
+            } else if (fIndex < firstHalf.length) {
+                destination[fIndex + sIndex] = firstHalf[fIndex];
+                fIndex++;
+            } else if (sIndex < secondHalf.length) {
+                destination[fIndex + sIndex] = secondHalf[sIndex];
+                sIndex++;
+            }
+        }
+        return new InversionsResult(splitInversionsCount, destination);
+    }
+
+    private static class InversionsResult {
+        final int count;
+        final int[] sortedItems;
+
+        private InversionsResult(int count, int[] sortedItems) {
+            this.count = count;
+            this.sortedItems = sortedItems;
+        }
     }
 
 }
